4394a0038cd4a287a006112cb80fc0c192a82433
Arth <arth.dharaskar97@gmail.com>
2018-06-22 18:45:47 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
-var pop_size = 20;
+var pop_size = 10;
 var paddles = [];
 var prevPaddles = [];
 var ball;
+var flag = 0;
+
 function setup(){
       createCanvas(600,400);
       for (var i =pop_size;i>=0;i--) {
@@ -11,32 +13,40 @@ function setup(){
 }
 function draw(){
       background(150);
-      if(paddles.length ==0){
-          paddles = newGeneration(prevPaddles, pop_size);
-          console.log("New gen");
+      if(paddles.length ===0 && flag ===0){
+          var values;
+          values = newGeneration(prevPaddles, pop_size);
+          paddles = values[0];
+          // console.log(values[1]);
+          prevPaddles=[];
+          flag = 1;
+          // console.log(paddles);
       }
-
-      for (var i = pop_size;i>=0;i--) {
-        if(paddles.length>0)
-        {
-          if(ball.belowPaddle(paddles[i])){
-            prevPaddles.push(paddles.splice(i,1));
-          }
-          else{
-            paddles[i].think(ball);
-            paddles[i].update();
-          }
-        }
-      }
-      ball.update();
-
-
-      ball.show();
-      if(paddles.length > 0)
-      {
+      else{
         for (var i = pop_size;i>=0;i--) {
-          paddles[i].show();
+          if(paddles.length>0)
+          {
+            if(ball.belowPaddle(paddles[i])){
+              prevPaddles.push(paddles[i]);
+              paddles.splice(i,1);
+              if (paddles.length ===0){flag =0;}
+            }
+            else{
+              paddles[i].think(ball);
+              paddles[i].update();
+            }
+          }
         }
+        ball.update();
+
+        ball.show();
+        if(paddles.length > 0)
+        {
+          for (var i = pop_size;i>=0;i--) {
+            paddles[i].show();
+          }
+        }
+
       }
 
 }
