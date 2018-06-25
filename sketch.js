4f1d661136aa5a60992c5c8f6b45c5a516f2f472
Arth <arth.dharaskar97@gmail.com>
2018-06-25 22:43:18 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,49 +1,53 @@
-var pop_size = 10;
-var paddles = [];
+var pop_size = 30;
+var paddles_sketch = [];
 var prevPaddles = [];
 var ball;
 var flag = 0;
-
+var count = 1;
 function setup(){
       createCanvas(600,400);
-      for (var i =pop_size;i>=0;i--) {
-        paddles[i] = new Paddle();
+      for (var i =pop_size-1;i>=0;i--) {
+        paddles_sketch[i] = new Paddle();
       }
-      ball = new Ball(paddles,pop_size);
+      ball = new Ball(paddles_sketch,pop_size);
 }
 function draw(){
+      // console.log(paddles_sketch)
       background(150);
-      if(paddles.length ===0 && flag ===0){
+      if(paddles_sketch.length == 0 ){
           var values;
           values = newGeneration(prevPaddles, pop_size);
-          paddles = values[0];
-          // console.log(values[1]);
+          paddles_sketch = values[0];
+          ball = new Ball(paddles_sketch, pop_size);
           prevPaddles=[];
-          flag = 1;
-          // console.log(paddles);
       }
       else{
-        for (var i = pop_size;i>=0;i--) {
-          if(paddles.length>0)
-          {
-            if(ball.belowPaddle(paddles[i])){
-              prevPaddles.push(paddles[i]);
-              paddles.splice(i,1);
-              if (paddles.length ===0){flag =0;}
+        for (var i =paddles_sketch.length-1 ;i>=0; i--) {
+            if(ball.belowPaddle(paddles_sketch[i])){
+              prevPaddles.push(paddles_sketch.splice(i,1)[0]);
             }
             else{
-              paddles[i].think(ball);
-              paddles[i].update();
+              tf.tidy(() => {
+              paddles_sketch[i].think(ball);
+            });
+              paddles_sketch[i].update();
             }
+        }
+        for (var i in paddles_sketch) {
+          if(ball.hit_flag == 1 && paddles_sketch.fitness+paddles_sketch.score < 1)
+          {
+            console.log('occured');
+            prevPaddles.push(paddles_sketch.splice(i,1)[0]);
           }
         }
-        ball.update();
 
+
+        ball.update();
         ball.show();
-        if(paddles.length > 0)
+        if(paddles_sketch.length > 0)
         {
-          for (var i = pop_size;i>=0;i--) {
-            paddles[i].show();
+          for (var i in paddles_sketch) {
+            paddles_sketch[i].show();
           }
         }
 
