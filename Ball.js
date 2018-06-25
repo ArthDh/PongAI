19431b8a66aedc9572437b33656055c95c966f42
Arth <arth.dharaskar97@gmail.com>
2018-06-25 22:40:56 +0530

    Update Ball.js

--- Ball.js
+++ Ball.js
@@ -4,10 +4,12 @@ class Ball{
             this.pos = createVector(width/2, height/2);
             this.vel = createVector(random(-5,5),-5);
             this.acc = createVector();
-            this.maxspeed = 4;
+            this.maxspeed = 8;
             this.score = 0;
             this.paddles = paddles;
             this.pop_size= pop_size;
+            this.hit_flag = 0;
+
       }
       applyForce(force){
         this.acc.add(force);
@@ -19,6 +21,7 @@ class Ball{
         // this.vel.mult(0.95); //Resistance?
         if(this.pos.x<0|| this.pos.x>width-this.r/2){
           this.vel.x = this.vel.x*-1;
+
         }
         if(this.pos.y-this.r/2<0){
           this.vel.y = this.vel.y*-1;
@@ -28,30 +31,28 @@ class Ball{
             this.vel = createVector(0,-5);
             this.applyForce(-0.5);
             // console.log("HIT");
+            this.hit_flag =0;
             this.score= this.score+1;
 
         }
-
-        var flag=0;
-        if(paddles.length > 0)
+        // var flag=0;
+        if(this.paddles.length > 0)
         {
-          for(var i =0; i< this.pop_size; i++){
-            var paddle = paddles[i];
+          for(var i  in this.paddles){
+            var paddle = this.paddles[i];
+
             if(this.hitPaddle(paddle)){
-              paddle.fitness +=1;
-            }
-            if(this.hitPaddle(paddle) && flag===0){
-              flag = 1;
+              this.hit_flag =1;
               paddle.color = color(255,0,0);
               var angle = atan2((paddle.pos.x)-this.pos.x, paddle.pos.y-this.pos.y);
               if(cos(angle)>0){
                 this.vel.x = this.vel.x + this.vel.x*cos(angle);
                 this.vel.y = this.vel.y *-1;
+                break;
               }
-              else{
-                this.vel.y = -this.vel.y ;
-              }
-              // this.applyForce(-0.5);
+
+
+              this.vel.y = -2*this.vel.y ;
             }
           }
 
@@ -62,7 +63,8 @@ class Ball{
 
       hitPaddle(paddle){
           if(this.pos.x>paddle.pos.x && this.pos.x<paddle.pos.x+paddle.w && this.pos.y>paddle.pos.y-this.r/2 && this.pos.y< paddle.pos.y + paddle.h){
-            paddle.fitness +=1;
+            paddle.score +=1;
+            // paddle.fitness +=1;
             return true;
         }
         return false;
